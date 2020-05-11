770d7a1835274e831f3f4091f692e04581463fbd
xArthos <giampaololocascio@gmail.com>
2020-05-11 12:26:08 +0200

    More examples added

--- Giampaolo/Exercises/JavaScript/JavaScript Array/main.js
+++ Giampaolo/Exercises/JavaScript/JavaScript Array/main.js
@@ -1,4 +1,4 @@
-// variable type: let
+// variable types
 let language = 'English'; // String
 let isItOnline = true; // Boolean
 let moduleNumber = 12; // Number
@@ -18,7 +18,10 @@ let myArray = ['Arif', 'Adel', 'Asreen'];
 let myArray2 = new Array('Arif', 'Adel', 'Asreen');
 
 // Display Arrays in console
+console.log(typeof myArray) // Returns the type of the variable
 console.log(myArray);
+console.log(myArray[2]);
+console.log(typeof myArray2)
 console.log(myArray2);
 
 // Array with nested Arrays
@@ -34,3 +37,31 @@ classInfo = [
     myName = 'Giampaolo' // New variable declared inside the Array
 ];
 console.log(classInfo);
+
+// typeof returns as "object" also arrays and null variables
+let a = null;
+let b = [1, 2, 3];
+
+console.log(typeof a);
+console.log(typeof b);
+
+// Object is a Map/blueprint of anything
+/**
+ * object {
+ * key: value, 
+ * key: value, 
+ * key: value
+ * }
+ * 
+ * Keys are some kind of variables inside the object which should be unique inside the object
+ */
+
+let myObject = {
+    companyName: 'Digital Career Institute'
+};
+console.log(myObject);
+
+let myObject2 = {
+    companyName: 'DCI'
+};
+console.log(myObject2);
