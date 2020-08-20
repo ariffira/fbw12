4d109838c0c0f601945ede34cbfea50c0c874b70
xArthos <giampaololocascio@gmail.com>
2020-08-20 23:03:15 +0200

    Node.js exercise 20.08.20 (not yet finished)

--- Giampaolo/Exercises/Node.js/19.08.20/server.js
+++ Giampaolo/Exercises/Node.js/19.08.20/server.js
@@ -5,15 +5,28 @@ const fs = require('fs');
 
 let server = http.createServer((request, response) => {
     fs.readFile('index.html', (err, data) => {
-        if (err) {throw err} 
-        else {
-            response.writeHead(200, { 'Content-type': 'text/html' });
-            response.write(data);
-            response.end('Requested succesfully');
-        }
+        if (err) throw err;
+        response.writeHead(200, { 'Content-type': 'text/html' });
+        response.write(data);
+        response.end('Requested succesfully');
     })
 });
 
+fs.writeFile('textTest.txt', 'test', (err) => {
+    if (err) throw err;
+    console.log('New Text file');
+})
+
+fs.unlink('textTest.txt', (err) => {
+    if (err) throw err;
+    console.log('Text file Deleted')
+})
+
+fs.readFile('textTest.txt', (err, data) => {
+    if (err) throw err;
+    console.log(data)
+})
+
 server.listen(PORT, host, () => {
     console.log(`The server is running on ${host}:${PORT}`)
-})
+});
